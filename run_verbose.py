a3d7e7c12463ed276c03b58f9e9f679e42b4d1d9
Vishnu P <vishnup2603@gmail.com>
2026-03-24 11:30:02 +0530

    chore: purge root-level scratch scripts and enforce strict root-level .gitignore

--- run_verbose.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import time
-
-proc = subprocess.Popen(["python", "-v", "-c", "import app.main"], stdout=open("verbose.txt", "w"), stderr=subprocess.STDOUT)
-time.sleep(10)
-proc.terminate()
